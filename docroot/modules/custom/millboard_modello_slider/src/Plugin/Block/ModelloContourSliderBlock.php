aae64623c52e9596464ad9b512e98bd8521e6f96
Hashkite <sargar.akshay96@gmail.com>
2025-02-13 12:04:27 +0530

    Added images

--- docroot/modules/custom/millboard_modello_slider/src/Plugin/Block/ModelloContourSliderBlock.php
+++ docroot/modules/custom/millboard_modello_slider/src/Plugin/Block/ModelloContourSliderBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\millboard_modello_slider\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Provides a block for displaying Millboard Modello slider data.
@@ -14,59 +15,34 @@ use Drupal\Core\Block\BlockBase;
  */
 class ModelloContourSliderBlock extends BlockBase {
 
+  use StringTranslationTrait;
+
   /**
    * {@inheritdoc}
    */
   public function build() {
     // Define an array of image paths for the slider (top-image and nav-images)
-    $images = [
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.5..jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/contour/Design 11.13..jpg',
-      ],
+    $indices = [
+      'Design 11.1', 'Design 11.2', 'Design 11.3', 'Design 11.4', 'Design 11.5', 'Design 11.6', 'Design 11.7', 'Design 11.11', 'Design 11.12', 'Design 11.13', 'Design 11.14',
+      'Design 11.15', 'Design 11.16', 'Design 11.17', 'Design 11.21', 'Design 11.22', 'Design 11.23', 'Design 11.24', 'Design 11.25', 'Design 11.26', 'Design 11.27',
+      'Design 11.28', 'Design 11.31', 'Design 11.32', 'Design 11.33', 'Design 11.34', 'Design 11.35', 'Design 11.36', 'Design 11.37', 'Design 11.38', 'Design 11.41',
+      'Design 11.42', 'Design 11.43', 'Design 11.44', 'Design 11.45', 'Design 11.46', 'Design 11.47', 'Design 11.48', 'Design 11.51', 'Design 11.52', 'Design 11.53',
+      'Design 11.54', 'Design 11.55', 'Design 11.56', 'Design 11.57', 'Design 11.58', 'Design 11.61', 'Design 11.62', 'Design 11.63', 'Design 11.64', 'Design 11.65',
+      'Design 11.66', 'Design 11.67', 'Design 11.71', 'Design 11.72',
     ];
 
+    $images = array_map(function ($index) {
+      return [
+        'top_image' => "/modules/custom/millboard_modello_slider/img/contour/$index.jpg",
+        'nav_image' => "/modules/custom/millboard_modello_slider/img/contour/$index.jpg",
+      ];
+    }, $indices);
+
     $slider_html = '<div class="coh-style-container-small coh-style-inner-container-padding laying-pattern">
-      <h3 class="text-align-center"><strong>' . t('CONTOUR LAYING PATTERNS') . '</strong></h3>
+      <h3 class="text-align-center"><strong>' . $this->t('CONTOUR LAYING PATTERNS') . '</strong></h3>
       <p class="coh-style-green-dash" style="margin:0 auto;">&nbsp;</p>
       <p class="text-align-center p1">&nbsp;</p>
-      <p class="text-align-center p1">' . t('With Modello, the possibilities are endless. By simply offsetting the same board, you can create a stunning array of over 50 unique patterns, each with its own distinctive character. From refined symmetry to bold, dynamic configurations. Modello empowers you to craft a design thats uniquely yours. Whether you choose Linear or Contour, this revolutionary product lets you bring your vision to life, redefining what decking can achieve.') . '</p>
+      <p class="text-align-center p1">' . $this->t('With Modello, the possibilities are endless. By simply offsetting the same board, you can create a stunning array of over 50 unique patterns, each with its own distinctive character. From refined symmetry to bold, dynamic configurations. Modello empowers you to craft a design thats uniquely yours. Whether you choose Linear or Contour, this revolutionary product lets you bring your vision to life, redefining what decking can achieve.') . '</p>
       </div>';
     $slider_html .= '<div class="contour-slider coh-container coh-style-container-wrapper"><div class="slick-top-image slider-for">';
     foreach ($images as $image) {
@@ -80,9 +56,9 @@ class ModelloContourSliderBlock extends BlockBase {
     }
     $nav_html .= '</div></div>';
     $nav_html .= '<div class="coh-style-container-small coh-style-inner-container-padding laying-pattern-footer">';
-    $nav_html .= '<h3 class="text-align-center"><strong>' . t('OVER 50 POSSIBLE PATTERNS') . '</strong></h3>';
+    $nav_html .= '<h3 class="text-align-center"><strong>' . $this->t('OVER 50 POSSIBLE PATTERNS') . '</strong></h3>';
     $nav_html .= '<p class="text-align-center">&nbsp;</p>';
-    $nav_html .= '<p class="text-align-center"><a class="coh-style-primary-button-with-millboard-green-bg" style="margin:0px auto;" href="#">' . t('GOT A QUESTION?') . '</a></p>';
+    $nav_html .= '<p class="text-align-center"><a class="coh-style-primary-button-with-millboard-green-bg" style="margin:0px auto;" href="#">' . $this->t('GOT A QUESTION?') . '</a></p>';
     $nav_html .= '</div>';
 
     // Combine the top image slider and nav images.
--- docroot/modules/custom/millboard_modello_slider/src/Plugin/Block/ModelloLinearSliderBlock.php
+++ docroot/modules/custom/millboard_modello_slider/src/Plugin/Block/ModelloLinearSliderBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\millboard_modello_slider\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Provides a block for displaying Millboard Modello slider data.
@@ -14,59 +15,32 @@ use Drupal\Core\Block\BlockBase;
  */
 class ModelloLinearSliderBlock extends BlockBase {
 
+  use StringTranslationTrait;
+
   /**
    * {@inheritdoc}
    */
   public function build() {
     // Define an array of image paths for the slider (top-image and nav-images)
-    $images = [
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
-      [
-        'top_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.34.jpg',
-        'nav_image' => '/modules/custom/millboard_modello_slider/img/linear/Design 1.24.jpg',
-      ],
+    $indices = [
+      'Design 1.1', 'Design 1.2', 'Design 1.3', 'Design 1.4', 'Design 1.5', 'Design 1.6', 'Design 1.7', 'Design 1.11', 'Design 1.12', 'Design 1.13', 'Design 1.14', 'Design 1.15', 'Design 1.16', 'Design 1.17', 'Design 1.21',
+      'Design 1.22', 'Design 1.23', 'Design 1.24', 'Design 1.25', 'Design 1.26', 'Design 1.27', 'Design 1.28', 'Design 1.31', 'Design 1.37', 'Design 1.38', 'Design 1.41', 'Design 1.42', 'Design 1.43', 'Design 1.44',
+      'Design 1.45', 'Design 1.46', 'Design 1.47', 'Design 1.48', 'Design 1.51', 'Design 1.52', 'Design 1.53', 'Design 1.54', 'Design 1.55', 'Design 1.56', 'Design 1.57', 'Design 1.58', 'Design 1.61', 'Design 1.62',
+      'Design 1.63', 'Design 1.64', 'Design 1.65', 'Design 1.66', 'Design 1.67', 'Design 1.71', 'Design 1.72',
     ];
 
+    $images = array_map(function ($index) {
+      return [
+        'top_image' => "/modules/custom/millboard_modello_slider/img/linear/$index.jpg",
+        'nav_image' => "/modules/custom/millboard_modello_slider/img/linear/$index.jpg",
+      ];
+    }, $indices);
+
     $slider_html = '<div class="coh-style-container-small coh-style-inner-container-padding laying-pattern">
-    <h3 class="text-align-center"><strong>' . t('LINEAR LAYING PATTERNS') . '</strong></h3>
+    <h3 class="text-align-center"><strong>' . $this->t('LINEAR LAYING PATTERNS') . '</strong></h3>
     <p class="coh-style-green-dash" style="margin:0 auto;">&nbsp;</p>
     <p class="text-align-center p1">&nbsp;</p>
-    <p class="text-align-center p1">' . t('With Modello, the possibilities are endless. By simply offsetting the same board, you can create a stunning array of over 50 unique patterns, each with its own distinctive character. From refined symmetry to bold, dynamic configurations. Modello empowers you to craft a design thats uniquely yours. Whether you choose Linear or Contour, this revolutionary product lets you bring your vision to life, redefining what decking can achieve.') . '</p>
+    <p class="text-align-center p1">' . $this->t('With Modello, the possibilities are endless. By simply offsetting the same board, you can create a stunning array of over 50 unique patterns, each with its own distinctive character. From refined symmetry to bold, dynamic configurations. Modello empowers you to craft a design thats uniquely yours. Whether you choose Linear or Contour, this revolutionary product lets you bring your vision to life, redefining what decking can achieve.') . '</p>
     </div>';
     $slider_html .= '<div class="Linear-slider coh-container coh-style-container-wrapper"><div class="slick-top-image slider-for">';
     foreach ($images as $image) {
@@ -76,13 +50,13 @@ class ModelloLinearSliderBlock extends BlockBase {
 
     $nav_html = '<div class="slick-nav-images slider-nav">';
     foreach ($images as $image) {
-      $nav_html .= '<div class="nav-image"><img src="' . $image['nav_image'] . '" alt="Nav Image"></div>';
+      $nav_html .= '<div class="nav-image"><img src="' . $image['nav_image'] . '" width="100" height="100" alt="Nav Image"></div>';
     }
     $nav_html .= '</div></div>';
     $nav_html .= '<div class="coh-style-container-small coh-style-inner-container-padding laying-pattern-footer">';
-    $nav_html .= '<h3 class="text-align-center"><strong>' . t('OVER 50 POSSIBLE PATTERNS') . '</strong></h3>';
+    $nav_html .= '<h3 class="text-align-center"><strong>' . $this->t('OVER 50 POSSIBLE PATTERNS') . '</strong></h3>';
     $nav_html .= '<p class="text-align-center">&nbsp;</p>';
-    $nav_html .= '<p class="text-align-center"><a class="coh-style-primary-button-with-millboard-green-bg" style="margin:0px auto;" href="#">' . t('GOT A QUESTION?') . '</a></p>';
+    $nav_html .= '<p class="text-align-center"><a class="coh-style-primary-button-with-millboard-green-bg" style="margin:0px auto;" href="#">' . $this->t('GOT A QUESTION?') . '</a></p>';
     $nav_html .= '</div>';
 
     // Combine the top image slider and nav images.
